c0f83759ef61345cac756a9f86899739a3bc4d27
zack lore <zacktron@gmail.com>
2017-01-10 17:56:59 -0800

    Updated Test Window
    Changed to Texture2D method.  XY coordinates for pixel is still off but
    I almost have the drawing worked out.

--- Assets/Editor/TestWindo.cs
+++ Assets/Editor/TestWindo.cs
@@ -12,46 +12,129 @@ public class TestWindo : EditorWindow {
         window.titleContent = title;
     }
 
-    Color currentColor;
+    Color CurrentColor;
     Rect[] pixels;
     Color[] colors;
+    Texture2D CurrentImage = null;
+    Rect ImageArea;
+
+    #region BrushSize
+    private int _brushSize;
+    public int BrushSize
+    {
+        get
+        {
+            return _brushSize;
+        }
+        set
+        {
+            if (value != _brushSize)
+            {
+                _brushSize = value * (ZoomSize > 0 ? ZoomSize : 1);
+            }
+        }
+    }
+    #endregion
+
+    #region ImageSize
+    private int _imageSize;
+    public int ImageSize
+    {
+        get
+        {
+            return _imageSize;
+        }
+        set
+        {
+            if (value != _imageSize)
+            {
+                _imageSize = value;
+                adjustedSize = ImageSize * ZoomSize;
+                UpdateImageSize();
+            }
+        }
+    }
+    #endregion
+
+    #region ZoomSize
+    private int _zoomSize;
+    public int ZoomSize
+    {
+        get
+        {
+            return _zoomSize;
+        }
+        set
+        {
+            if (value != _zoomSize)
+            {
+                _zoomSize = value;
+            }
+        }
+    }
+    #endregion
+
+    int adjustedSize = 0;
+    int col = 0;
+    int row = 0;
+    int positionBuffer = 100;
 
     private void OnEnable()
     {
-        currentColor = new Color(0, 0, 0);
-        pixels = new Rect[24 * 24];
-        colors = new Color[pixels.Length];
+        //adjustedSize = 32 * 4;
+        //pixels = new Rect[24 * 24];
+        //colors = new Color[pixels.Length];
+
+        ZoomSize = 4;
+        CurrentColor = new Color(0, 0, 0);
+        UpdateImageSize();        
     }
 
     private void OnGUI()
     {
-        int adustedSize = 24 * 4;
-        Vector3 MousePos = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+        ImageSize = EditorGUILayout.IntField("Image Size: ", ImageSize);
+        BrushSize = EditorGUILayout.IntField("Brush Size: ", BrushSize);
+        //adjustedSize = ImageSize * BrushSize;
 
-        GUI.Box(new Rect(100, 100, adustedSize, adustedSize), GUIContent.none);
-        EditorGUILayout.IntField("Size: ", 24);
-        currentColor = EditorGUILayout.ColorField("Color: ", currentColor, null);
-        EditorGUI.DrawRect(new Rect(MousePos.x, MousePos.y - position.y, 4, 4), currentColor);
-        Repaint();
-                
+        Vector3 MousePos = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+        //GUI.Box(ImageArea, GUIContent.none);
+        EditorGUI.DrawPreviewTexture(ImageArea, CurrentImage);
+
+        CurrentColor = EditorGUILayout.ColorField("Color: ", CurrentColor, null);
+        EditorGUI.DrawRect(new Rect(MousePos.x, MousePos.y - position.y, BrushSize, BrushSize), CurrentColor);
+
+        //CurrentImage.SetPixel(0, 0, Color.red);
+        
         if (Event.current.type == EventType.MouseDown)//detect mousedown event
         {
-            int col = (int)((MousePos.x - position.x) - 100) % adustedSize;
-            int row = (int)((MousePos.y - position.y) - 100) % adustedSize;
+            //int col = (int)((MousePos.x - position.x) - 100) % adustedSize;
+            //int row = (int)((MousePos.y - position.y) - 100) % adustedSize;
 
-            row = row / 4;
-            col = col / 4;
+            //row = row / 4;
+            //col = col / 4;
 
-            Debug.Log("MousePos: " + MousePos + " " + row + ", " + col);
-            int entry = (row * 24) + col;
-            if (entry <= pixels.Length)
-            {
-                Debug.Log("Draw Pixel");
-                pixels[entry] = new Rect(MousePos.x, MousePos.y, 4, 4);
-                colors[entry] = new Color(currentColor.r, currentColor.g, currentColor.b, currentColor.a);
-                EditorGUI.DrawRect(pixels[entry], colors[entry]);
-            }
+            //int col = (int)((MousePos.x - position.x) - 100) % 32;
+            //int col = (int)MousePos.x - 100;
+            //int row = (int)((MousePos.y - position.y) - 100) % 32;
+
+            //int entry = (row * 24) + col;
+            //if (entry <= pixels.Length)
+            //{
+            Debug.Log("MousePos: " + MousePos);
+            //pixels[entry] = new Rect(MousePos.x, MousePos.y, 4, 4);
+            //colors[entry] = new Color(currentColor.r, currentColor.g, currentColor.b, currentColor.a);
+            //EditorGUI.DrawRect(pixels[entry], colors[entry]);
+
+            //CurrentImage.SetPixel(col, CurrentImage.height - row, currentColor);
+            int x = (int)MousePos.x - positionBuffer;
+            int y = (int)(MousePos.y - (-MousePos.y)) - positionBuffer;
+            CurrentImage.SetPixel((int)MousePos.x - positionBuffer, (int)MousePos.y - positionBuffer, CurrentColor);
+
+            //CurrentImage.SetPixels(col, row, 4, 0, new Color[3] { Color.blue, Color.red, Color.white });
+            CurrentImage.Apply();
+            //}
         }
+        Repaint();
 
         //Draw Colors
         //for (int i = 0; i < pixels.Length; i++)
@@ -62,5 +145,11 @@ public class TestWindo : EditorWindow {
         //        Debug.Log(colors[i]);
         //    }
         //}
+    }
+
+    private void UpdateImageSize()
+    {
+        CurrentImage = new Texture2D(adjustedSize, adjustedSize);
+        ImageArea = new Rect(100, 100, CurrentImage.height, CurrentImage.width);
     }
 }
